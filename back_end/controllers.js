a81996fb2a2f4168cef43f5fc47bf1825778476d
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-07-27 14:53:57 -0600

    initial back structure

--- back_end/controllers.js
+++ back_end/controllers.js
@@ -13,11 +13,21 @@ exports.getAllProducts((res,req)=>{
 })
 
 exports.getProductById((res,req)=>{
-  models.retrieveProducts()
+  models.retrieveProductById()
     .then((data)=>{
       res.send(data).status(200).end();
     })
     .catch((err)=>{
-      console.log('There is an error in controllers getAllProducts: ', err);
+      console.log('There is an error in controllers getProductById: ', err);
+    })
+})
+
+exports.getProductStylesById((res,req)=>{
+  models.retrieveStylesByProductId()
+    .then((data)=>{
+      res.send(data).status(200).end();
+    })
+    .catch((err)=>{
+      console.log('There is an error in controllers getProductStylesById: ', err);
     })
 })
--- back_end/models.js
+++ back_end/models.js
@@ -1,11 +1,17 @@
-
 //query's and transactions with DB live here:
+const db = require('./db')
 
 
 
-//query to find all products
+
+
+//query to find first 5 products
+exports.retrieveProducts()
+
 
 //query's to find a product by id
+exports.retrieveProductById()
 
 //query's to find a products styles by id
+exports.retrieveStylesByProductId()
 
